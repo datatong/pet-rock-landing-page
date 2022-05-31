2d657c229b9b8c09ab20a95acc63939b84e52461
datatong <59157188+datatong@users.noreply.github.com>
2022-05-30 22:57:41 -0700

    Add files via upload

--- app.js
+++ app.js
@@ -11,9 +11,31 @@ const size = galleryImages[0].clientWidth;
 gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
 
 //Button Listeners
-nextBtn.addEventListener('click', ()=> {
+nextBtn.addEventListener('click', () => {
+    if (counter >= galleryImages.length -1) return;
     gallerySlide.style.transition = "transform 0.4s ease-in-out";
     counter++;
-    //console.log(counter);
-    
+
+    gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+prevBtn.addEventListener('click', () => {
+    if (counter <= 0) return;
+    gallerySlide.style.transition = "transform 0.4s ease-in-out";
+    counter--;
+
+    gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+});
+
+gallerySlide.addEventListener('transitioned', () => {
+    if (galleryImages[counter].id === 'lastClone') {
+        gallerySlide.style.transition = "none";
+        counter = galleryImages.length -2;
+        gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
+    if (galleryImages[counter].id === 'firstClone') {
+        gallerySlide.style.transition = "none";
+        counter = galleryImages.length -counter;
+        gallerySlide.style.transform = 'translateX(' + (-size * counter) + 'px)';
+    }
 });
